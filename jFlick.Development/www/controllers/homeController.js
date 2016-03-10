c2ef9c6ef7ff24c9a70f453729c1480f43d7953f
あまみや ゆうこ <1@1234.sh>
2016-03-10 15:26:11 +0800

    Small: Enhance the demo

--- jFlick.Development/www/controllers/homeController.js
+++ jFlick.Development/www/controllers/homeController.js
@@ -1,8 +1,9 @@
 router.get('/home/index', function (req, res, next) {
-    // alert('Hello world!');
+    //alert('Hello world!');
 });
 
 router.get('/home/page', function (req, res, next) {
+    //alert('name=' + req.query.name);
     res.find('#btnCalc').click(function () {
         var a = parseInt(res.find('#txtA').val());
         var b = parseInt(res.find('#txtB').val());
